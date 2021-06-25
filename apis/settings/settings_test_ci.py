db769527004f056ca9f5e99b26e929facd25287d
steffres <stefan.resch@oeaw.ac.at>
2021-06-25 12:47:46 +0200

    removed dev db config

--- apis/settings/settings_test_ci.py
+++ apis/settings/settings_test_ci.py
@@ -50,15 +50,9 @@ INSTALLED_APPS = [
     "apis_highlighter",
 ]
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'apis_db_dev',
-		'USER': 'apis_user',
-		'PASSWORD': 'apis_password',
-		'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-		'PORT': '3306',
-	}
-}
+DATABASES = {}
+
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
 LANGUAGE_CODE = "de"
+
